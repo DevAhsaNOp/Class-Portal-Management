23f1fea079d43db06fd6002615487ae88d0d2919
Muhammad Ahsan <59480396+DevAhsaNOp@users.noreply.github.com>
2024-04-25 02:19:23 +0500

    Updated!

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Application.Interfaces.ClientInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController(IInstructor instructor,
+        IClass _class) : Controller
+    {
+        private readonly IClass _class = _class;
+        private readonly IInstructor _instructor = instructor;
+
+        [HttpGet]
+        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+        {
+            var instructorsAndClasses = new InstructorAndClassesVM
+            {
+                Classes = await _class.GetByStatuses(cancellationToken),
+                Instructors = await _instructor.GetByStatuses(cancellationToken)
+            };
+
+            return View(instructorsAndClasses);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> About(CancellationToken cancellationToken)
+        {
+            var Instructors = await _instructor.GetByStatuses(cancellationToken);
+            return View(Instructors);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Class(CancellationToken cancellationToken)
+        {
+            var Classes = await _class.GetByStatuses(cancellationToken);
+            return View(Classes);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Teachers(CancellationToken cancellationToken)
+        {
+            var Instructors = await _instructor.GetByStatuses(cancellationToken);
+            return View(Instructors);
+        }
+
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp/Models/InstructorAndClassesVM.cs
@@ -0,0 +1,11 @@
+﻿using Application.ClientFeatures.Class.Response;
+using Application.ClientFeatures.Instructor.Response;
+
+namespace WebApp.Models
+{
+    public class InstructorAndClassesVM
+    {
+        public List<ClassResponse> Classes { get; set; }
+        public List<InstructorResponseV2> Instructors { get; set; }
+    }
+}
